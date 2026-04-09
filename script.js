afea338b03347d445d6f4db43c4eba438d42f998
ConnorH2028 <connor.hamilton2028@gbwl.org>
2026-04-09 15:44:05 +0000

    Variables

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+// This is a SINGLELINE COMMENT
+
+/* This is a MULTI LINE COMMENT
+The code below is an example STATEMENT
+(instruction/command)
+Every statement ends with a SEMICOLON
+*/
+console.log("welcome to javascript!");
+
+// VARIABLES are named storage contaienrs for data (info)
+
+// DECLARE a variable  withe the key world LET
+let username;
+
+// ASSIGN a value to a named variable
+username = "connor";
+
+// DECLARE + ASSIGN a new variable in one line
+let pokemon = "zygarde"
+
+// See the value stored in a variable
+console.log(username);
+console.log(pokemon);
+
+// Store numbers in a variable
+let luckyNum = 13;
